b2a5ef87270ea0a793eb810676c7ac12c407ba0c
Tikhon <chabysow@gmail.com>
2023-07-28 13:32:18 +0300

    Revert "Refactor LockerBox.py"
    This reverts commit dce9e9c9ceccf130c586818cfbd364424db88df8.

--- LockerBox.py
+++ LockerBox.py
@@ -1,44 +1,49 @@
-import tkinter as tk
-import tkinter.tix as tkx
+from Data.DB_Lockers import Locker, Tenant, History, create_lickers
 from tkinter import messagebox as mb
 from tkcalendar import DateEntry
 from tkinter import ttk
+from tkinter import *
+from Display_views.display_lockers import rendering_lockers
+from Display_views.information_table import mainInfoBlock
+
+import tkinter.tix as tkx
+import tkinter as tk
 import datetime
 import sqlite3
 import time
 
-from Data.DB_Lockers import Locker, Tenant, History, create_lickers
-from Display_views.display_lockers import rendering_lockers
-from Display_views.information_table import mainInfoBlock
 
 # Файл лого для окон
 logo = 'Images/boxing.ico'
 
-# Параметры основного окна
-main_window = tk.Tk()
-main_window.resizable(False, False)
-main_window.title("Шкафчики")
-main_window.iconbitmap(logo)
-style = ttk.Style(main_window)
+# Параметры основного окна 
+root = Tk()
+root.resizable(False, False)
+root.title("Шкафчики")
+root.iconbitmap(logo)
+style = ttk.Style(root)
 style.configure("vista")
-main_window.state('zoomed')
+root.state('zoomed')
+ 
 
 # Переменные шаблонных png для отображения статуса
-photo = tk.PhotoImage(file=r"States/no_key/locker.png")
-photo_red = tk.PhotoImage(file=r"States/no_key/locker_red.png")
-photo_green = tk.PhotoImage(file=r"States/no_key/locker_green.png")
-photo_yellow = tk.PhotoImage(file=r"States/no_key/locker_yellow.png")
-photo_key = tk.PhotoImage(file=r"States/key/locker_key.png")
-photo_key_green = tk.PhotoImage(file=r"States/key/locker_key_green.png")
-photo_key_red = tk.PhotoImage(file=r"States/key/locker_key_red.png")
-photo_key_yellow = tk.PhotoImage(file=r"States/key/locker_key_yellow.png")
-
+photo = PhotoImage(file = r"States/no_key/locker.png")
+photo_red = PhotoImage(file = r"States/no_key/locker_red.png")
+photo_green = PhotoImage(file = r"States/no_key/locker_green.png")
+photo_yellow = PhotoImage(file = r"States/no_key/locker_yellow.png")
+photo_key = PhotoImage(file = r"States/key/locker_key.png") 
+photo_key_green = PhotoImage(file = r"States/key/locker_key_green.png")
+photo_key_red = PhotoImage(file = r"States/key/locker_key_red.png")
+photo_key_yellow = PhotoImage(file = r"States/key/locker_key_yellow.png")
+#--------------------------------------------------------------------------------
 context = [photo, photo_red, photo_green, photo_yellow, photo_key, photo_key_green, photo_key_red, photo_key_yellow]
+	
+""" ОТРИСОВКА ШКАФЧИКОВ """
 
-# ОТРИСОВКА ШКАФЧИКОВ
-rendering_lockers(main_window, context)
+rendering_lockers(root, context)
 
-# БЛОК ДОПОЛНИТЕЛЬНОЙ ИНФОРМАЦИИ
-mainInfoBlock(main_window)
 
-main_window.mainloop()
+"""БЛОК ДОПОЛНИТЕЛЬНОЙ ИНФОРМАЦИИ"""
+mainInfoBlock(root)
+
+root.mainloop()
